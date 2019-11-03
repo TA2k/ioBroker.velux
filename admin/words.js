0a81108a9d7b9dbfadaf18bad6b111cf9f17aea2
Ta2k <tombox2020@gmail.com>
2019-11-03 19:43:43 +0100

    intial release

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"velux adapter settings": {
+		"en": "Adapter settings for velux",
+		"de": "Adaptereinstellungen für velux",
+		"ru": "Настройки адаптера для velux",
+		"pt": "Configurações do adaptador para velux",
+		"nl": "Adapterinstellingen voor velux",
+		"fr": "Paramètres d'adaptateur pour velux",
+		"it": "Impostazioni dell'adattatore per velux",
+		"es": "Ajustes del adaptador para velux",
+		"pl": "Ustawienia adaptera dla velux",
+		"zh-cn": "velux的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
